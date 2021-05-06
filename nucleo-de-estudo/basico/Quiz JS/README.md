edef56a25c4af9dbe56eff9d902a033d258c570a
Monique <moniquesntsilva@gmail.com>
2021-05-06 20:37:10 -0300

    Atividade Quiz

---
+++ nucleo-de-estudo/basico/Quiz JS/README.md
@@ -0,0 +1,34 @@
+# Um quiz em HTML
+
+Colocando em prática os conceitos aprendidos de JS, DOM e HTML, construa um quiz simples.
+
+**Coordenador:** Pedro Mariotti
+
+## Instruções
+
+O seu quiz deve incluir:
+
+- Pelo menos 5 perguntas
+- Perguntas de pelo menos 3 tipos de input diferentes
+- Um botão de submit
+- Indicar quantas perguntas foram acertadas após o submit
+- Fazer alguma mudança visual no momento que for submetido
+
+Dica: Lembrem-se de usar os data-attributes! Eles podem ser muito úteis.
+
+## Materiais de Apoio
+
+- https://eloquentjavascript.net/
+- https://www.w3schools.com/howto/howto_html_file_upload_button.asp
+- https://developer.mozilla.org/pt-BR/docs/Web/Guide/HTML/Forms/Meu_primeiro_formulario_HTML
+- https://www.w3schools.com/cssref/css_selectors.asp
+
+## Como publicar a atividade
+
+1. Clone este repositório na sua máquina
+2. Crie uma branch com o número 1 juntamente com seu nome e sobrenome (ex: "1-joao-silva")
+3. Escreva todo o seu código nessa branch
+4. Verifique se todos os requisitos da atividade foram cumpridos
+5. Inclua no README.md os nomes das pessoas da sua equipe (se houver), pois é apenas uma branch por equipe
+6. Suba a sua branch para este repositório
+
---
+++ nucleo-de-estudo/basico/Quiz JS/script.js
@@ -0,0 +1,111 @@
+
+let Points = 0
+
+let QuestAPoints = 0
+
+
+
+const AnswerA = {"a":"Killua Zaoldyeck", "b":"Killua", "c":"Kilua", "d":"Killua Zoldyck", "e":"Kilua Zoldk"}
+
+document.querySelector("input#button").addEventListener("click", gerar_quiz)
+
+
+function gerar_quiz (){
+    let a = document.querySelector("#p1").value
+    let NomeJogador = document.querySelector("input#name").value
+
+/*Questão1*/
+
+Object.keys(AnswerA).forEach(function(key) {
+    AnswerA[key] == a ?  [++Points, ++QuestAPoints, ] : ''
+    if (QuestAPoints == 1){
+        document.querySelector("#p1").style.background="#57EB4B"
+    }
+    else {
+        document.querySelector("#p1").style.background="#c72e36cb"
+        document.querySelector("p#Resp1").innerHTML = `Killua Zaoldyeck`
+    }
+})
+
+/*Questão2*/
+
+if (document.querySelector("input#Verde").checked){
+    ++Points,
+    document.querySelector("div#quest2").style.background="#57EB4B"
+}
+else {
+    document.querySelector("div#quest2").style.background="#c72e36cb"
+    document.querySelector("p#Resp2").innerHTML = `Verde`
+}
+
+/*Questão3*/
+
+if (document.querySelector("input#Hunter").checked && document.querySelector("input#Quimera").checked && document.querySelector("input#Celestial").checked && document.querySelector("input#Wano").checked){
+    document.querySelector("div#quest3").style.background="#c72e36cb"
+    document.querySelector("p#Resp3").innerHTML = `Exame Hunter, Formigas Quimeras e Torre Celestial`
+}
+else if (document.querySelector("input#Hunter").checked && document.querySelector("input#Quimera").checked && document.querySelector("input#Celestial").checked){
+    ++Points
+    document.querySelector("div#quest3").style.background="#57EB4B"
+}
+else {
+    document.querySelector("div#quest3").style.background="#c72e36cb"
+    document.querySelector("p#Resp3").innerHTML = `Exame Hunter, Formigas Quimeras e Torre Celestial`
+}
+
+
+/*Questão4*/
+
+if (document.querySelector("input#Reforco").checked && document.querySelector("input#Transformacao").checked && document.querySelector("input#Especializacao").checked && document.querySelector("input#Emissao").checked){
+    document.querySelector("div#quest4").style.background="#c72e36cb"
+    document.querySelector("p#Resp4").innerHTML = `Reforço, Transformação e Emissão`
+}
+else if (document.querySelector("input#Reforco").checked && document.querySelector("input#Transformacao").checked && document.querySelector("input#Emissao").checked){
+    ++Points
+    document.querySelector("div#quest4").style.background="#57EB4B"
+}
+else {
+    document.querySelector("div#quest4").style.background="#c72e36cb"
+    document.querySelector("p#Resp4").innerHTML = `Reforço, Transformação e Emissão`
+}
+
+/*Questão5*/
+
+if (document.querySelector("input#Togashi").checked){
+    ++Points,
+    document.querySelector("div#quest5").style.background="#57EB4B"
+}
+else {
+    document.querySelector("div#quest5").style.background="#c72e36cb"
+    document.querySelector("p#Resp5").innerHTML = `Yoshihiro Togashi`
+}
+console.log(Points)
+
+
+/*Resultado*/
+
+if (Points == 5){
+    document.querySelector("div#ResultadoFinal").innerHTML = `<img src="./imagens/5-Netero.jpg" id="ImagemResultado"><p>Parabéns <strong>${NomeJogador}</strong>! A sua pontuação foi de 5 pontos, um acerto perfeito, digno do maior presidente da história da Associação Hunter, <strong>Isaac Netero</strong>.</p>`
+    document.querySelector("div#ResultadoFinal").style.border="#f9db6d solid 10px"
+}
+else if (Points == 4){
+    document.querySelector("div#ResultadoFinal").innerHTML = `<img src="./imagens/4-Hisoka.jpg" id="ImagemResultado"><p>Parabéns <strong>${NomeJogador}</strong>! A sua pontuação foi de 4 pontos, sem dúvidas um mestre do Nen. No universo de HxH você estaria próximo do nível de personagens monstruosos como <strong>Hisoka Morow</strong>.</p>`
+    document.querySelector("div#ResultadoFinal").style.border="#C92DF7 solid 10px"
+}
+else if (Points == 3){
+    document.querySelector("div#ResultadoFinal").innerHTML = `<img src="./imagens/3-Kite.jpg" id="ImagemResultado"><p>Parabéns <strong>${NomeJogador}</strong>! A sua pontuação foi de 3 pontos, seus conhecimentos já são acima da média, mas sua capacidade de crescimento é infinita ainda. Personagens como <strong>Kite</strong> representam suas habilidades.</p>`
+    document.querySelector("div#ResultadoFinal").style.border="#4188EB solid 10px"
+}
+else if (Points == 2){
+    document.querySelector("div#ResultadoFinal").innerHTML = `<img src="./imagens/2-Gon.jpg" id="ImagemResultado"><p>É...Parabéns <strong>${NomeJogador}</strong>. A sua pontuação foi de 2 pontos, você tem conhecimento básico do universo HxH. Mas pense pelo lado positivo, assim como <strong>Gon Freecss</strong>, você ainda tem todo um continente de materiais para aprender e poder crescer até o topo!</p> `
+    document.querySelector("div#ResultadoFinal").style.border="#14FC40 solid 10px"
+}
+else if (Points == 1){
+    document.querySelector("div#ResultadoFinal").innerHTML = `<img src="./imagens/1-Zushi.jpg" id="ImagemResultado"><p><strong>${NomeJogador}</strong>, você fez 1 ponto apenas. Mas não se envergonhe, assim como <strong>Zushi</strong>, se você continuar treinando/lendo poderá um dia se tornar o maior conhecedor e retornar o teste, acertando as 5 questões!</p> `
+    document.querySelector("div#ResultadoFinal").style.border="#E3E0DC solid 10px"
+}
+else if (Points == 0){
+    document.querySelector("div#ResultadoFinal").innerHTML = `<img src="./imagens/0-Tompa.png" id="ImagemResultado"><p>Caramba <strong>${NomeJogador}</strong>, 0 pontos? Provavelmente você não viu nada dessa obra prima chamada HunterXHunter, que coisa feia. Volte quando você conseguir identificar esse personagem da foto acima e você será uma pessoa mais iluminada.</p> `
+    document.querySelector("div#ResultadoFinal").style.border="#F63902 solid 10px"
+}
+}
