d1f23d75f7fa63d64c1f1a70ae82672ab4abb909
Monique <moniquesntsilva@gmail.com>
2021-07-09 01:37:37 -0300

    config funcionalidade

--- nucleo-de-estudo/basico/Calculadora/app.js
+++ nucleo-de-estudo/basico/Calculadora/app.js
@@ -8,7 +8,10 @@ let operations = ["+", "-", "*", "/", "%", "="];
 let clear = ["C", "CE"];
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 let digits = [];
-let result = [];
+let currentNumber = "";
+let clickedOperation = [];
+let sequence = [];
+let result = "";
 
 input.forEach((e) => {
   e.addEventListener("click", (event) => {
@@ -22,19 +25,36 @@ input.forEach((e) => {
 const handleClear = (value) => {
   if (value === "C") {
     digits = [];
+    sequence = [];
     return (display.innerText = "0");
   } else {
-    digits.pop();
-    return (display.innerText = digits.join(""));
+    if (digits[0] === [] || display.innerText === "0") {
+      return (display.innerText = "0");
+    } else {
+      digits.pop();
+    }
   }
+  return (display.innerText = digits.join(""));
 };
+
 const handleNumbers = (value) => {
   digits.push(value);
-  display.innerText = digits.join("");
-};
-const handleOperation = (value) => {
-  digits.push(value);
+  currentNumber = digits.join("");
   display.innerText = digits.join("");
 };
 
+const handleOperation = (value) => {
+  clickedOperation.push(value);
+  sequence.push(currentNumber);
+  currentNumber = 0;
+  if (value === "=") {
+    handleEqual(sequence);
+    digits = [];
+  } else {
+    digits.push(value);
+    display.innerText = digits.join("");
+  }
+};
+
+const handleEqual = (number) => {};
 //21 words max
